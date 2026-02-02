3de174efe321446d73682d81a1af6fdb610cc29c
hanserwei <hanserwei@qq.com>
2026-02-02 21:17:56 +0800

    feat(auth): 初始化认证模块及项目基础配置
    - 新增认证模块pom文件，配置项目基本属性和依赖
    - 新增父项目pom，统一管理版本和依赖
    -创建认证启动类HannoteAuthApplication
    - 配置Spring Boot应用名、端口和线程参数
    - 添加项目启动Banner文件
    - 配置IDEA相关文件及编码为UTF-8
    - 添加.gitignore规则，忽略目标文件、IDE配置和系统文件等

---
+++ hannote-auth/src/main/java/com/hanserwei/hannote/auth/HannoteAuthApplication.java
@@ -0,0 +1,14 @@
+package com.hanserwei.hannote.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hanser
+ */
+@SpringBootApplication
+public class HannoteAuthApplication {
+    static void main() {
+        SpringApplication.run(HannoteAuthApplication.class);
+    }
+}
